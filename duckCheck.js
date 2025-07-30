2bbfa3ab1d91cea68039386c942e58f8b44d6efe
Rob LeGrand <roblegrand98@gmail.com>
2025-07-29 19:38:13 -0500

    Reduce duckCheck function to one return statement; add more tests

--- duckCheck.js
+++ duckCheck.js
@@ -1,33 +1,48 @@
 /*jslint devel */
 /*jshint esnext: true */
-// https://jsbin.com/cevilopiqe/edit?js,console
+// https://jsbin.com/jaravezida/edit?js,console
+// old: https://jsbin.com/cevilopiqe/edit?js,console
 
 (function () {
    'use strict';
 
    const duckCheck = function duckCheck(objectToCheck, prototypeObject) {
-      if (
-         typeof objectToCheck !== typeof prototypeObject
-         || Array.isArray(objectToCheck) !== Array.isArray(prototypeObject)
-      ) {
-         return false;
-      }
-      if (Array.isArray(prototypeObject)) {
-         return prototypeObject.every(function (value, index) {
-            return objectToCheck.hasOwnProperty(index) && duckCheck(objectToCheck[index], value);
-         });
-      }
-      if (typeof prototypeObject === 'object') {
-         return Object.keys(prototypeObject).every(function (prop) {
-            return objectToCheck.hasOwnProperty(prop) && duckCheck(objectToCheck[prop], prototypeObject[prop]);
-         });
-      }
-      return true;
+      return (
+         (
+            typeof objectToCheck !== typeof prototypeObject
+            || Array.isArray(objectToCheck) !== Array.isArray(prototypeObject)
+         )
+         ? false
+         : Array.isArray(prototypeObject)
+         ? prototypeObject.every((value, index) => (
+            objectToCheck.hasOwnProperty(index)
+            && duckCheck(objectToCheck[index], value)
+         ))
+         : typeof prototypeObject === 'object'
+         ? Object.keys(prototypeObject).every((prop) => (
+            objectToCheck.hasOwnProperty(prop)
+            && duckCheck(objectToCheck[prop], prototypeObject[prop])
+         ))
+         : true
+      );
    };
 
    console.log(duckCheck(3, 4));
    console.log(duckCheck(3, '4'));
    console.log(duckCheck(3, {}));
    console.log(duckCheck({}, {}));
+   console.log(duckCheck({}, []));
    console.log(duckCheck({tr: 1}, {tr: 'g'}));
+   console.log(duckCheck(
+      {c: 'sry', tr: [1, true, {}]},
+      {c: 'lan', tr: [2, false, {}]}
+   ));
+   console.log(duckCheck(
+      {c: 'sry', tr: [1, true, {}]},
+      {c: 'lan', tr: [2, false]}
+   ));
+   console.log(duckCheck(
+      {c: 'sry', tr: [1, true]},
+      {c: 'lan', tr: [2, false, {}]}
+   ));
 }());
