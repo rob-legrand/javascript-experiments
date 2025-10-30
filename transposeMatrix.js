3512c681b18c84ce9973b25de81b64363c744a05
Rob LeGrand <roblegrand98@gmail.com>
2025-10-30 15:37:23 -0500

    Comment and test versions of transposeArray more thoroughly

--- transposeMatrix.js
+++ transposeMatrix.js
@@ -1,21 +1,21 @@
 /*jslint devel */
 /*jshint esnext: true */
-// https://jsbin.com/dutiyudiya/edit?js,console
-// old: https://jsbin.com/kalaqezogo/edit?js,console
+// https://jsbin.com/gayipojubo/edit?js,console
+// old: https://jsbin.com/dutiyudiya/edit?js,console
+// older: https://jsbin.com/kalaqezogo/edit?js,console
 
 (function () {
    'use strict';
-   let arr;
 
-   const transposeArrayA = (a) => (
-      a[0].map((ignore, whichColumn) => (
-         a.map((row) => (
-            row[whichColumn]
-         ))
-      ))
+   // functiony indentation
+   const transposeMatrix1 = (a) => a[0].map(
+      (ignore, whichColumn) => a.map(
+         (row) => row[whichColumn]
+      )
    );
 
-   const transposeArrayB = (a) => (
+   // double indentation
+   const transposeMatrix2 = (a) => (
       a[0].map(
          (ignore, whichColumn) => (
             a.map(
@@ -27,34 +27,32 @@
       )
    );
 
-   const transposeArrayC = (a) => a[0].map(
-      (ignore, whichColumn) => a.map(
-         (row) => row[whichColumn]
-      )
+   // loopy indentation
+   const transposeMatrix3 = (a) => (
+      a[0].map((ignore, whichColumn) => (
+         a.map((row) => (
+            row[whichColumn]
+         ))
+      ))
    );
 
-   arr = [
+   const arr33 = [
       [1, 2, 3],
       [4, 5, 6],
       [7, 8, 9]
    ];
-   console.log(arr);
-   console.log(transposeArrayA(arr));
+   console.log(arr33);
+   console.log(transposeMatrix1(arr33));
+   console.log(transposeMatrix2(arr33));
+   console.log(transposeMatrix3(arr33));
 
-   arr = [
-      [1, 2, 3],
-      [4, 5, 6],
-      [7, 8, 9],
-      [10, 11, 12]
-   ];
-   console.log(arr);
-   console.log(transposeArrayB(arr));
-
-   arr = [
+   const arr34 = [
       [1, 2, 3, 4],
       [5, 6, 7, 8],
       [9, 10, 11, 12]
    ];
-   console.log(arr);
-   console.log(transposeArrayC(arr));
+   console.log(arr34);
+   console.log(transposeMatrix1(arr34));
+   console.log(transposeMatrix2(arr34));
+   console.log(transposeMatrix3(arr34));
 }());
