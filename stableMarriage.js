32ce746e67c7270d5f56e2e7c455d93b00ec044f
Rob LeGrand <roblegrand98@gmail.com>
2025-11-01 17:53:18 -0500

    Modernize JavaScript style in stableMarriage.js

--- stableMarriage.js
+++ stableMarriage.js
@@ -1,14 +1,15 @@
-/*jslint devel: true, indent: 3 */
-// https://jsbin.com/noyipotuho/edit?js,console
+/*jslint devel */
+/*jshint esnext: true */
+// https://jsbin.com/kimeceheni/edit?js,console
+// old: https://jsbin.com/noyipotuho/edit?js,console
 
 (function () {
    'use strict';
-   var createGaleShapleyMatching, flipMatching, randomizePrefs;
 
-   createGaleShapleyMatching = function (askersPrefs, askedsPrefs) {
-      var isEveryoneMatched, isPersonMatched, offerStatusMatrix;
+   const createGaleShapleyMatching = function (askersPrefs, askedsPrefs) {
+      let offerStatusMatrix;
 
-      isEveryoneMatched = function (offerStatusMatrix) {
+      const isEveryoneMatched = function (offerStatusMatrix) {
          return Object.keys(offerStatusMatrix).every(function (asker) {
             return Object.keys(offerStatusMatrix[asker]).some(function (asked) {
                return offerStatusMatrix[asker][asked] === 'matched';
@@ -16,12 +17,16 @@
          });
       };
 
-      isPersonMatched = function (offerStatusMatrix, person) {
-         return offerStatusMatrix.hasOwnProperty(person) ? Object.keys(offerStatusMatrix[person]).some(function (otherPerson) {
-            return offerStatusMatrix[person][otherPerson] === 'matched';
-         }) : Object.keys(offerStatusMatrix).some(function (otherPerson) {
-            return offerStatusMatrix[otherPerson][person] === 'matched';
-         });
+      const isPersonMatched = function (offerStatusMatrix, person) {
+         return (
+            offerStatusMatrix.hasOwnProperty(person)
+            ? Object.keys(offerStatusMatrix[person]).some(function (otherPerson) {
+               return offerStatusMatrix[person][otherPerson] === 'matched';
+            })
+            : Object.keys(offerStatusMatrix).some(function (otherPerson) {
+               return offerStatusMatrix[otherPerson][person] === 'matched';
+            })
+         );
       };
 
       offerStatusMatrix = Object.keys(askersPrefs).reduce(function (statusMatrix, asker) {
@@ -32,28 +37,29 @@
          return statusMatrix;
       }, {});
       (function keepMatching() {
-         if (isEveryoneMatched(offerStatusMatrix)) {
-            return;
-         }
-         Object.keys(askersPrefs).forEach(function (asker) {
-            var haventAskedYet;
-            if (!isPersonMatched(offerStatusMatrix, asker)) {
-               haventAskedYet = askersPrefs[asker].filter(function (asked) {
-                  return offerStatusMatrix[asker][asked] === 'none';
+         if (!isEveryoneMatched(offerStatusMatrix)) {
+            Object.keys(askersPrefs).forEach(function (asker) {
+               if (!isPersonMatched(offerStatusMatrix, asker)) {
+                  const haventAskedYet = askersPrefs[asker].filter(function (asked) {
+                     return offerStatusMatrix[asker][asked] === 'none';
+                  });
+                  offerStatusMatrix[asker][haventAskedYet[0]] = 'matched';
+               }
+            });
+            Object.keys(askedsPrefs).forEach(function (asked) {
+               const offers = askedsPrefs[asked].filter(function (asker) {
+                  return offerStatusMatrix[asker][asked] === 'matched';
                });
-               offerStatusMatrix[asker][haventAskedYet[0]] = 'matched';
-            }
-         });
-         Object.keys(askedsPrefs).forEach(function (asked) {
-            var offers;
-            offers = askedsPrefs[asked].filter(function (asker) {
-               return offerStatusMatrix[asker][asked] === 'matched';
+               offers.forEach(function (asker, which) {
+                  offerStatusMatrix[asker][asked] = (
+                     which > 0
+                     ? 'rejected'
+                     : 'matched'
+                  );
+               });
             });
-            offers.forEach(function (asker, which) {
-               offerStatusMatrix[asker][asked] = which > 0 ? 'rejected' : 'matched';
-            });
-         });
-         keepMatching();
+            keepMatching();
+         }
       }());
       return Object.keys(askersPrefs).reduce(function (matching, asker) {
          matching[asker] = Object.keys(offerStatusMatrix[asker]).filter(function (asked) {
@@ -63,59 +69,58 @@
       }, {});
    };
 
-   flipMatching = function (matching) {
+   const flipMatching = function (matching) {
       return Object.keys(matching).reduce(function (flippedMatching, person) {
          flippedMatching[matching[person]] = person;
          return flippedMatching;
       }, {});
    };
 
-   randomizePrefs = function (prefs) {
-      var newPrefs, oldPrefs;
-      oldPrefs = Array.from(prefs);
-      newPrefs = [];
-      while (oldPrefs.length > 0) {
-         newPrefs = newPrefs.concat(oldPrefs.splice(Math.floor(Math.random() * oldPrefs.length), 1));
-      }
-      return newPrefs;
+   const randomizePrefs = function (prefs) {
+      return prefs.reduce(function (soFar) {
+         const whichChosen = Math.floor(Math.random() * soFar.prefsLeft.length);
+         return {
+            randomizedPrefs: soFar.randomizedPrefs.concat(soFar.prefsLeft[whichChosen]),
+            prefsLeft: soFar.prefsLeft.slice(0, whichChosen).concat(soFar.prefsLeft.slice(whichChosen + 1))
+         };
+      }, {
+         randomizedPrefs: [],
+         prefsLeft: prefs
+      }).randomizedPrefs;
    };
 
-   (function () {
-      var mensPrefs, womensPrefs;
-
-      womensPrefs = {
-         charlotte: ['bingley', 'darcy', 'collins', 'wickham'],
-         elizabeth: ['wickham', 'darcy', 'bingley', 'collins'],
-         jane: ['bingley', 'wickham', 'darcy', 'collins'],
-         lydia: ['bingley', 'wickham', 'darcy', 'collins']
-      };
-
-      mensPrefs = {
-         bingley: ['jane', 'elizabeth', 'lydia', 'charlotte'],
-         collins: ['jane', 'elizabeth', 'lydia', 'charlotte'],
-         darcy: ['elizabeth', 'jane', 'charlotte', 'lydia'],
-         wickham: ['lydia', 'jane', 'elizabeth', 'charlotte']
-      };
-
+   const testPrefs = function (womensPrefs, mensPrefs) {
       console.log(womensPrefs);
       console.log(mensPrefs);
       console.log(createGaleShapleyMatching(womensPrefs, mensPrefs));
       console.log(flipMatching(createGaleShapleyMatching(womensPrefs, mensPrefs)));
       console.log(createGaleShapleyMatching(mensPrefs, womensPrefs));
+   };
 
-      womensPrefs = Object.keys(womensPrefs).reduce(function (newPrefs, person) {
-         newPrefs[person] = randomizePrefs(womensPrefs[person]);
-         return newPrefs;
-      }, {});
-      mensPrefs = Object.keys(mensPrefs).reduce(function (newPrefs, person) {
-         newPrefs[person] = randomizePrefs(mensPrefs[person]);
-         return newPrefs;
-      }, {});
+   const womensOriginalPrefs = {
+      charlotte: ['bingley', 'darcy', 'collins', 'wickham'],
+      elizabeth: ['wickham', 'darcy', 'bingley', 'collins'],
+      jane: ['bingley', 'wickham', 'darcy', 'collins'],
+      lydia: ['bingley', 'wickham', 'darcy', 'collins']
+   };
 
-      console.log(womensPrefs);
-      console.log(mensPrefs);
-      console.log(createGaleShapleyMatching(womensPrefs, mensPrefs));
-      console.log(flipMatching(createGaleShapleyMatching(womensPrefs, mensPrefs)));
-      console.log(createGaleShapleyMatching(mensPrefs, womensPrefs));
-   }());
+   const mensOriginalPrefs = {
+      bingley: ['jane', 'elizabeth', 'lydia', 'charlotte'],
+      collins: ['jane', 'elizabeth', 'lydia', 'charlotte'],
+      darcy: ['elizabeth', 'jane', 'charlotte', 'lydia'],
+      wickham: ['lydia', 'jane', 'elizabeth', 'charlotte']
+   };
+
+   testPrefs(womensOriginalPrefs, mensOriginalPrefs);
+
+   testPrefs(
+      Object.keys(womensOriginalPrefs).reduce(function (newPrefs, person) {
+         newPrefs[person] = randomizePrefs(womensOriginalPrefs[person]);
+         return newPrefs;
+      }, {}),
+      Object.keys(mensOriginalPrefs).reduce(function (newPrefs, person) {
+         newPrefs[person] = randomizePrefs(mensOriginalPrefs[person]);
+         return newPrefs;
+      }, {})
+   );
 }());
